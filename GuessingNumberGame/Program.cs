b5c1a166052fecbf2a919cd4e7f27fa8a86eb5e8
Liseth Cardozo Sejas <cardozoliseth@gmail.com>
2018-03-07 14:46:49 -0800

    add basic methods

--- GuessingNumberGame/Program.cs
+++ GuessingNumberGame/Program.cs
@@ -10,6 +10,27 @@ namespace GuessingNumberGame
     {
         static void Main(string[] args)
         {
+            
         }
+
+        public void WelcomeMessage() 
+        {
+           Console.WriteLine("Welcome to guessing number!");
+        }
+        public string UserInput()
+        {
+            Console.WriteLine("Guess a number between 1 to 100");
+            string userGuess = Console.ReadLine();
+            return userGuess;
+        }
+        public void Results(int number)
+        {
+
+        }
+        public int RandomNumber()
+        {
+            return 1; 
+        }
+
     }
 }
